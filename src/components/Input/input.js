2d487466143b532d16d29bc6de2d07e1dd2a436a
tunacim <tunaz783@gmail.com>
2023-03-18 20:37:28 +0300

    Used Formik

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { TextInput,View } from "react-native";
 import styles from "./Input.style"
-const Input=({placeholder,onChangeText})=>{
+const Input=({placeholder,onChangeText,value})=>{
 return(
     <View style={styles.container}>
-        <TextInput placeholder={placeholder} onChangeText={onChangeText}></TextInput>
+        <TextInput placeholder={placeholder} onChangeText={onChangeText} value={value}></TextInput>
     </View>
 )
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,29 @@
 import React from "react";
 import {View,Image,SafeAreaView} from "react-native"
+import { Formik } from "formik";
+
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input";
 import styles from "./Login.style"
+
 const Login=()=>{
+    function handleLogin(value){
+        console.log(value)
+
+    }
     return(
         <SafeAreaView style={styles.container}>
         <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../../assets/login_logo.png")}/>
         </View>
-        <View style={styles.bodyContainer}>
-        <Input placeholder="Kullanıcı adını giriniz"></Input>
-            <Input placeholder="Şifrenizi giriniz"></Input>
-            <Button text="Giriş yap"></Button>
-        </View>
+        <Formik initialValues={{username:"",password:""}} onSubmit={handleLogin}>
+            
+            {({handleSubmit,handleChange,values})=>(<View style={styles.bodyContainer}>
+            <Input placeholder="Kullanıcı adını giriniz" value={values.username} onChangeText={handleChange("username")}></Input>
+            <Input placeholder="Şifrenizi giriniz" value={values.password} onChangeText={handleChange("password")}></Input>
+            <Button text="Giriş yap" onPress={handleSubmit}></Button>
+            </View>)}
+         </Formik>
         </SafeAreaView>
     )
 }
